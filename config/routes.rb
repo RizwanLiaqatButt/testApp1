f49a097c41b738ee23e78aaa7276b98d6e9ecf7a
bsef12m002 <bsef12m002@pucit.edu.pk>
2016-01-01 00:59:52 +0000

    categories,modeland test..

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
+
+  resources :categories, except: [:destroy]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
